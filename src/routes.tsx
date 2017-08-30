2fcc81c72b73ecb0182d10ccaaa50b73a780695e
John Sabath <johnsabath@carfax.com>
2017-08-30 15:46:05 -0500

    Removing component examples

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import { BrowserRouter, Link, Route } from "react-router-dom";
-import ExampleConnectedRouteComponent from "./components/ExampleConnectedRouteComponent";
 
 export default (
   <BrowserRouter>
     <div>
-      <Route path="/" exact={true} component={ExampleConnectedRouteComponent} />
-      <Route path="/:message" component={ExampleConnectedRouteComponent} />
+      <Route path="/" exact={true} />
     </div>
   </BrowserRouter>
 );
