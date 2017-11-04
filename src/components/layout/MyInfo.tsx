e6946db73ddff259dd987e51951de8f390b644ca
Bradey Whitlock <bradeywhitlock@carfax.com>
2017-11-04 13:15:37 -0500

    lol

--- src/components/layout/MyInfo.tsx
+++ src/components/layout/MyInfo.tsx
@@ -6,7 +6,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="myInfo">
-        <div data-aos="fade">hi</div>
+        <div data-aos="fade">Morgan is dumb</div>
       </div>
     );
   }
